bc659a246f9ba3c2eda6f8afed03cfd09a45a437
Philipp Engelhard <philippengelhard@gmail.com>
2019-08-23 17:01:37 +0200

    fixed uneven scaling of noise

--- src/generation/perlinnoiseelevationprovider.cpp
+++ src/generation/perlinnoiseelevationprovider.cpp
@@ -16,11 +16,12 @@ PerlinNoiseElevationProvider::PerlinNoiseElevationProvider(unsigned int seed)
 double PerlinNoiseElevationProvider::elevationAt(const double x, const double y, const double z)
 {
 	double elevation = m_e0 * this->noiseElevation(x, y, z);
-	elevation += m_e1 *	this->noiseElevation(x * 2,  y * 2,  z);
-	elevation += m_e2 *	this->noiseElevation(x * 4,  y * 4,  z);
-	elevation += m_e3 *	this->noiseElevation(x * 8,  y * 8,  z);
-	elevation += m_e4 *	this->noiseElevation(x * 16, y * 16, z);
-	elevation += m_e5 *	this->noiseElevation(x * 32, y * 32, z);
+	elevation += m_e1 *	this->noiseElevation(x * 2,  y * 2,  z * 2);
+	elevation += m_e2 *	this->noiseElevation(x * 4,  y * 4,  z * 4);
+	elevation += m_e3 *	this->noiseElevation(x * 8,  y * 8,  z * 8);
+	elevation += m_e4 *	this->noiseElevation(x * 16, y * 16, z * 16);
+	elevation += m_e5 *	this->noiseElevation(x * 32, y * 32, z * 32);
+
 
 	return elevation;
 }
